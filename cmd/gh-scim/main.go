cbdcd2ede330818a46e2908710d36d5059b9fbf7
Matt Todd <chiology@gmail.com>
2018-01-26 14:05:43 -0500

    Add gh-scim CLI tool
    Rough sketch at basic SCIM client. Some bits hardcoded.

---
+++ cmd/gh-scim/main.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+// gh-scim <command> -o <org> [guid]
+//
+// <org> is the organization name, e.g. `acme`, and is required for all commands.
+//
+// commands
+// * list
+// * remove [guid]
+//   [guid] is required
+// * add ...
+
+const defaultAPIURL = "https://api.github.com"
+
+type apiClient struct {
+	client  *http.Client
+	baseURL string
+	token   string
+	org     string
+}
+
+func (c *apiClient) buildRequest(method, endpoint string) (*http.Request, error) {
+	req, err := http.NewRequest(method, c.buildEndpointURL(endpoint), nil)
+	// http.NewRequest
+
+	req.Header.Set("Accept", "application/vnd.github.cloud-9-preview+json+scim")
+	req.Header.Set("Authorization", "Bearer "+c.token)
+
+	if method == "POST" {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	return req, err
+}
+
+func (c *apiClient) buildEndpointURL(path string) string {
+	return fmt.Sprintf("%s%s", c.baseURL, path)
+}
+
+func (c *apiClient) do(req *http.Request) (*http.Response, error) {
+	// log.Print(req)
+	return c.client.Do(req)
+}
+
+// {"schemas":["urn:ietf:params:scim:api:messages:2.0:ListResponse"],"totalResults":0,"itemsPerPage":0,"startIndex":1,"Resources":[]}
+// { "schemas":["urn:ietf:params:scim:api:messages:2.0:ListResponse"],
+//   "totalResults":2,
+//   "itemsPerPage":2,
+//   "startIndex":1,
+//   "Resources":[
+//     {
+//       "schemas":["urn:ietf:params:scim:schemas:core:2.0:User"],
+//       "id":"e7818cf4-0206-11e8-8526-afbcdd6f73fd",
+//       "externalId":"evilmtodd",
+//       "userName":"evilmtodd",
+//       "name":{"givenName":"Mtodd","familyName":"Evil"},
+//       "emails":[{"value":"chiology+evilmtodd@gmail.com","type":"work","primary":true}],
+//       "active":true,
+//       "meta":{
+// "resourceType":"User","created":"2018-01-25T14:35:31-05:00","lastModified":"2018-01-25T14:35:31-05:00","location":"https://api.github.com/scim/v2/organizations/GH4B/Users/e7818cf4-0206-11e8-8526-afbcdd6f73fd"}},
+//     {"schemas":["urn:ietf:params:scim:schemas:core:2.0:User"],"id":"d9ff5ddc-0200-11e8-88b5-b07393a906c1","externalId":"00udrnla8jKyYID8F0h7","userName":"mtodd@github.com","name":{"givenName":"Matt","familyName":"Todd"},"emails":[{"value":"mtodd@github.com","primary":true,"type":"work"}],"active":true,"meta":{"resourceType":"User","created":"2018-01-25T13:52:11-05:00","lastModified":"2018-01-25T13:52:25-05:00","location":"https://api.github.com/scim/v2/organizations/GH4B/Users/d9ff5ddc-0200-11e8-88b5-b07393a906c1"}}
+//   ]
+// }
+type scimListResponse struct {
+	Schemas      []string `json:"schemas"`
+	TotalResults int      `json:"totalResults"`
+	ItemsPerPage int      `json:"itemsPerPage"`
+	StartIndex   int      `json:"startIndex"`
+	Resources    []scimResource
+}
+
+// {
+//   "schemas":["urn:ietf:params:scim:schemas:core:2.0:User"],
+//   "id":"e7818cf4-0206-11e8-8526-afbcdd6f73fd",
+//   "externalId":"evilmtodd",
+//   "userName":"evilmtodd",
+//   "name":{"givenName":"Mtodd","familyName":"Evil"},
+//   "emails":[{"value":"chiology+evilmtodd@gmail.com","type":"work","primary":true}],
+//   "active":true,
+//   "meta":{"resourceType":"User","created":"2018-01-25T14:35:31-05:00","lastModified":"2018-01-25T14:35:31-05:00","location":"https://api.github.com/scim/v2/organizations/GH4B/Users/e7818cf4-0206-11e8-8526-afbcdd6f73fd"}
+// }
+type scimResource struct {
+	Schemas    []string      `json:"schemas"`
+	ID         string        `json:"id"`
+	ExternalID string        `json:"externalId"`
+	UserName   string        `json:"userName"`
+	Name       interface{}   `json:"name"`
+	Emails     []interface{} `json:"emails"`
+	Active     bool          `json:"active"`
+}
+
+// GET https://api.github.com/scim/v2/organizations/:organization/Users
+func listHandler(client *apiClient, filter string) error {
+	req, err := client.buildRequest("GET", fmt.Sprintf("/scim/v2/organizations/%s/Users", client.org))
+	if err != nil {
+		return err
+	}
+
+	if len(filter) > 0 {
+		q := req.URL.Query()
+		q.Add("filter", url.QueryEscape(filter))
+		req.URL.RawQuery = q.Encode()
+	}
+
+	res, err := client.do(req)
+	if err != nil {
+		return err
+	}
+
+	// log.Println(res)
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusBadRequest {
+		return fmt.Errorf("list: bad request: %s", string(body))
+	}
+
+	if res.StatusCode == http.StatusNotFound {
+		return fmt.Errorf("list: not found: %s", string(body))
+	}
+
+	// log.Printf("%v", body)
+	// log.Println(string(body))
+
+	var list scimListResponse
+	if err := json.Unmarshal(body, &list); err != nil {
+		return err
+	}
+
+	// log.Printf("%v", list)
+
+	// var f interface{}
+	// if err := json.Unmarshal(body, &f); err != nil {
+	// 	return err
+	// }
+	// log.Printf("%v", f)
+
+	for _, user := range list.Resources {
+		json, err := json.Marshal(user)
+		if err != nil {
+			return err
+		}
+		// log.Printf("%s", json)
+		fmt.Println(string(json))
+	}
+
+	return nil
+}
+
+// DELETE /scim/v2/organizations/:organization/Users/:id
+func removeHandler(client *apiClient, guid string) error {
+	req, err := client.buildRequest("DELETE", fmt.Sprintf("/scim/v2/organizations/%s/Users/%s", client.org, guid))
+	if err != nil {
+		return err
+	}
+
+	res, err := client.do(req)
+	if err != nil {
+		return err
+	}
+
+	// log.Println(res) // debugging
+
+	if res.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("remove failed: %v", res)
+	}
+
+	log.Printf("removed %s", guid)
+	return nil
+}
+
+const addScimUserTmpl = `
+{
+  "schemas":["urn:ietf:params:scim:schemas:core:2.0:User"],
+  "externalId":"evilmtodd",
+  "userName":"evilmtodd",
+  "name":{
+    "familyName":"Evil",
+    "givenName":"Mtodd"
+  },
+  "emails":[
+    {
+      "value":"chiology+evilmtodd@gmail.com",
+      "type":"work",
+      "primary":true
+    }
+  ]
+}
+`
+
+// {
+//   "schemas":["urn:ietf:params:scim:schemas:core:2.0:User"],
+//   "id":"e7818cf4-0206-11e8-8526-afbcdd6f73fd",
+//   "externalId":"evilmtodd",
+//   "userName":"evilmtodd",
+//   "name":{"givenName":"Mtodd","familyName":"Evil"},
+//   "emails":[{"value":"chiology+evilmtodd@gmail.com","type":"work","primary":true}],
+//   "active":true,
+//   "meta":{"resourceType":"User","created":"2018-01-25T14:35:31-05:00","lastModified":"2018-01-25T14:35:31-05:00","location":"https://api.github.com/scim/v2/organizations/GH4B/Users/e7818cf4-0206-11e8-8526-afbcdd6f73fd"}
+// }
+type scimUser struct {
+	scimResource
+}
+
+func addHandler(client *apiClient) error {
+	req, err := client.buildRequest("POST", fmt.Sprintf("/scim/v2/organizations/%s/Users", client.org))
+	if err != nil {
+		return err
+	}
+
+	req.Body = ioutil.NopCloser(bytes.NewBufferString(addScimUserTmpl))
+
+	// log.Printf("%v", req)
+
+	res, err := client.do(req)
+	if err != nil {
+		return err
+	}
+
+	// log.Printf("%v", res)
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		return fmt.Errorf("remove failed: %v", res)
+	}
+
+	// log.Printf("%v", body)
+	// log.Println(string(body))
+
+	var user scimUser
+	if err := json.Unmarshal(body, &user); err != nil {
+		return err
+	}
+
+	log.Printf("added: %s", user.ID)
+
+	return nil
+}
+
+func main() {
+	var err error
+
+	// configuration
+	token := os.Getenv("TOKEN")
+	org := os.Getenv("ORG")
+
+	// HTTP client
+	client := &apiClient{
+		client:  &http.Client{},
+		baseURL: defaultAPIURL,
+		token:   token,
+		org:     org,
+	}
+
+	cmds := os.Args[1:]
+	// log.Printf("%v", cmds)
+	if len(cmds) < 1 {
+		log.Fatal("command required")
+	}
+
+	switch cmds[0] {
+	case "list":
+		// TODO: support filter
+		var filter string
+		if len(cmds) > 1 {
+			filter = cmds[1]
+		}
+
+		err = listHandler(client, filter)
+	case "remove":
+		if len(cmds) < 2 {
+			log.Fatal("guid is required")
+		}
+
+		guid := cmds[1]
+		err = removeHandler(client, guid)
+	case "add":
+		err = addHandler(client)
+	default:
+		log.Fatal("commands are: list, remove, add")
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
